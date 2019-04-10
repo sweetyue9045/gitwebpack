6d17374801c7e4b163b055b39bfc200ce475fdf3
sweetyue9045 <sweetyue9045@gmail.com>
2019-04-10 20:52:35 +0800

    css finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,25 @@ import $ from 'jquery';
 import bird from '../images/bird.jpg';
 import mouse from '../images/squirrel.jpg';
 
+import '../css/style.css';
+
+import "../css/fonts/PT-Sans-Bold.eot";
+import "../css/fonts/PT-Sans-Bold.svg";
+import "../css/fonts/PT-Sans-Bold.ttf";
+import "../css/fonts/PT-Sans-Bold.woff";
+import "../css/fonts/PT-Sans-BoldItalic.eot";
+import "../css/fonts/PT-Sans-BoldItalic.svg";
+import "../css/fonts/PT-Sans-BoldItalic.ttf";
+import "../css/fonts/PT-Sans-BoldItalic.woff";
+import "../css/fonts/PT-Sans-Italic.eot";
+import "../css/fonts/PT-Sans-Italic.woff";
+import "../css/fonts/PT-Sans-Italic.ttf";
+import "../css/fonts/PT-Sans-Italic.svg";
+import "../css/fonts/PT-Sans.eot";
+import "../css/fonts/PT-Sans.svg";
+import "../css/fonts/PT-Sans.ttf";
+import "../css/fonts/PT-Sans.woff";
+
 $('#img_bird').attr("src", bird);
 $('#img_mouse').attr("src", mouse);
 
